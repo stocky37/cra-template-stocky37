e4c38604a5aacaa368e46411809be4e664685e09
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-10 23:22:23 +0100

    configure absolute imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './index.css';
 
+import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
