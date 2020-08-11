03a08aebd269e09959f05e708a29b10f3a09c903
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-11 10:52:43 +0100

    add json-server for mocking requests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,7 +12,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<Routes>
-					<Route path="/:name">
+					<Route path="/:nameId">
 						<Hello />
 					</Route>
 				</Routes>
--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router';
 
 const Hello = () => {
-	const {name} = useParams();
-	return <p>Hello, {name || 'World'}!</p>;
+	const {nameId} = useParams();
+	const [name, setName] = useState(undefined);
+
+	useEffect(() => {
+		fetch(`http://localhost:8080/names/${nameId}`)
+			.then((res) => res.json())
+			.then((json) => {
+				setName(json.name);
+			});
+	}, [nameId]);
+
+	return <p>Hello, {name || nameId}!</p>;
 };
 
 export default Hello;
