e3302a0939c131a5b1115a12243fbd6b05302c03
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-12 02:04:12 +0100

    rename some files after copying

--- bin/build.js
+++ bin/build.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const fs = require('fs-extra');
 const ignore = require('ignore');
 
 const ignorer = ignore().add(['node_modules', 'package*.json', '.idea', 'api/live.json']);
+const renames = {
+	'.gitignore': 'gitignore',
+};
 
 async function copyTemplateFiles(appDir, templateDir) {
 	return fs.copy(appDir, templateDir, {
@@ -12,8 +16,18 @@ async function copyTemplateFiles(appDir, templateDir) {
 	});
 }
 
+async function applyRenames(rootDir) {
+	const promises = [];
+	Object.keys(renames).forEach((key) => {
+		promises.push(fs.move(path.join(rootDir, key), path.join(rootDir, renames[key])));
+	});
+	return Promise.all(promises);
+}
+
 function createTemplate(appDir = 'app', templateDir = 'template') {
-	copyTemplateFiles(appDir, templateDir).then();
+	copyTemplateFiles(appDir, templateDir).then(async () => {
+		await applyRenames(templateDir);
+	});
 }
 
 createTemplate();
