244decbe6e169416ea636c33ca2ffd77148768f3
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-15 00:54:29 +0100

    all source package.json set to 0.1.0 with patch upgrade incoming

--- scripts/showversion.js
+++ scripts/showversion.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
 const fs = require('fs-extra');
 
+// display the version of the workspace that calls this script
+process.chdir(process.argv[2]);
 return fs.readJson('package.json').then((pkg) => console.log(pkg.version));
