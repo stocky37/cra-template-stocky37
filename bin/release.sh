69b5fdc422f040bf6deaccecf2caaa3497a77194
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-15 00:34:27 +0100

    fix publish scripts

--- bin/release.sh
+++ bin/release.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-repo="$1"
+DEFAULT_TAG_TEMPLATE='v${NEW_VERSION}'
+DEFAULT_COMMIT_TEMPLATE='release ${TAG}'
 
+format() {
+  local templatestr="$1"; shift
+  echo "$templatestr" | envsubst
+}
 
-version=$(yarn version:show)
+echo "Discovering new version..."
+export NEW_VERSION=$(yarn version:show)
+export TAG="$(format "${RELEASE_TAG_TEMPLATE:-$DEFAULT_TAG_TEMPLATE}")"
+
+echo "Releasing $TAG..."
 yarn version apply --all
-
 git remote add release "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-git tag "${TAG_PREFIX:-v}${version}"
-git push --tags
+git commit -am "$(format "${RELEASE_COMMIT_TEMPLATE:-$DEFAULT_COMMIT_TEMPLATE}")"
+git tag "$TAG"
+git push release
+git push release --tags
 
+echo "Publishing $TAG..."
+yarn publish
