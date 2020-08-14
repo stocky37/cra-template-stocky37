a99cf7ba05940d6a56edcde7ca78ccdf2f3fb47a
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-08-14 19:32:45 +0100

    fix template tests

--- packages/template-stocky37/src/components/App/App.test.js
+++ packages/template-stocky37/src/components/App/App.test.js
@@ -1,10 +1,15 @@
 import {render} from '@testing-library/react';
 import React from 'react';
+import {MemoryRouter} from 'react-router';
 
 import App from './App';
 
 test('renders learn react link', () => {
-	const {getByText} = render(<App />);
+	const {getByText} = render(
+		<MemoryRouter>
+			<App />
+		</MemoryRouter>
+	);
 	const linkElement = getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
